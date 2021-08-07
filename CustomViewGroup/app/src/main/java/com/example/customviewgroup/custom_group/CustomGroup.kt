0c4b80b8d004a0f87d460da697a42800c1faddbe
PoisonousHeart <poisonousheart@yandex.ru>
2021-08-08 00:36:30 +0300

    add listener on textview click inside custom view group

--- CustomViewGroup/app/src/main/java/com/example/customviewgroup/custom_group/CustomGroup.kt
+++ CustomViewGroup/app/src/main/java/com/example/customviewgroup/custom_group/CustomGroup.kt
@@ -2,6 +2,7 @@ package com.example.customviewgroup.custom_group
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.TextView
@@ -25,6 +26,9 @@ class CustomGroup : Childs, LinearLayoutCompat{
 
     private fun initView(context: Context){
         binding = CustomGroupBinding.inflate(LayoutInflater.from(context),  this, true)
+        binding.tex.setOnClickListener {
+            Log.d("из кастомного: ", "custom")
+        }
     }
 
     override fun getTextView() = binding.tex
