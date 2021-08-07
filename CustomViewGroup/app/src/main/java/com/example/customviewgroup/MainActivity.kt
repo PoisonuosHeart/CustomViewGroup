af5ced1d33941f6e6b781b286e4151ac0ffd8769
PoisonousHeart <poisonousheart@yandex.ru>
2021-08-07 22:29:46 +0300

    create custom view group

--- CustomViewGroup/app/src/main/java/com/example/customviewgroup/MainActivity.kt
+++ CustomViewGroup/app/src/main/java/com/example/customviewgroup/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.customviewgroup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.customviewgroup.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        binding.item = "bbbb"
     }
 }
---
+++ CustomViewGroup/app/src/main/java/com/example/customviewgroup/custom_group/interfaces/Childs.kt
@@ -0,0 +1,8 @@
+package com.example.customviewgroup.custom_group.interfaces
+
+import android.widget.TextView
+
+interface Childs {
+    val textV: TextView
+    fun getTextView(): TextView
+}
